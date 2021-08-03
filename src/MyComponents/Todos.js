1ce3fe477c1709956df7bce747a10800e3a41736
Neeraj <salunkeneeraj@gmail.com>
2021-08-03 23:38:57 +0530

    b1 commit

--- src/MyComponents/Todos.js
+++ src/MyComponents/Todos.js
@@ -6,7 +6,7 @@ function Todos(props) {
         <div className="container">
             <h3 className="text-center">Todos List</h3> 
             {/* <Todo todo={props.todos[0]}/> */}
-            {props.todos.length == 0? "No todos to display":
+            {props.todos.length == 0? "No todos to display right now":
               props.todos.map(todo => { /* to use loops in JSX,we use map. Know more:- https://www.telerik.com/blogs/beginners-guide-loops-in-react-jsx */
                                        /* todo in above line refers to first task of todos object made in App.js */
                 return <Todo todo={todo} key={todo.sno} onDelete={props.onDelete}/>
